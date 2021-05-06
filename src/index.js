e21ff3dabca69f9b3f8aa6207bb8f544f31763c4
mariovarela99@outlook.com <mariovarela99@outlook.com>
2021-05-06 22:27:12 +0100

    :rocket: Second Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,80 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import { EditorState } from "draft-js";
+import { Editor } from "react-draft-wysiwyg";
+import "./App.css";
+import "../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
-ReactDOM.render(
+function uploadImageCallBack(file) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:3333/files");
+    const data = new FormData();
+    data.append("image", file);
+    xhr.send(data);
+    xhr.addEventListener("load", () => {
+      const response = JSON.parse(xhr.responseText);
+      console.log(response);
+      resolve(response);
+    });
+    xhr.addEventListener("error", () => {
+      const error = JSON.parse(xhr.responseText);
+      console.log(error);
+      reject(error);
+    });
+  });
+}
+
+class EditorContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editorState: EditorState.createEmpty(),
+    };
+  }
+
+  onEditorStateChange = (editorState) => {
+    // console.log(editorState)
+    this.setState({
+      editorState,
+    });
+  };
+
+  render() {
+    const { editorState } = this.state;
+    return (
+      <div className="editor">
+        <Editor
+          editorState={editorState}
+          onEditorStateChange={this.onEditorStateChange}
+          placeholder="Type Something..."
+          toolbar={{
+            inline: { inDropdown: true },
+            list: { inDropdown: true },
+            textAlign: { inDropdown: true },
+            link: { inDropdown: true },
+            history: { inDropdown: true },
+            image: {
+              uploadCallback: uploadImageCallBack,
+              alt: { present: true, mandatory: true },
+            },
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+const App = () => (
+  <div>
+    <h2> Rich Editor Text With Wysiwyg and Draft.js </h2>
+    <EditorContainer />
+  </div>
+);
+
+render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
